a56399c0f636eb644a539948aa1f53c0e3112584
Attili Venkatesh <attili_venkatesh@apple.com>
2020-04-02 16:02:39 +0530

    <rdar://problem/59039283> Refetch Reload Handler Modal scroll is cut off and not visible

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void  main(String args){
-        System.out.println(":::::<<<<hello JGIT!!!!>>>>:::::::");
+        System.out.println("!!!!!hello JGIT!!!!>>>>:::::::");
     }
 }
